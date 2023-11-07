5aaf76195e3206b0afa0d62b1a9a36f59d052ace
ChoByungHyun <cbj973@gmail.com>
2023-11-07 21:16:30 +0900

    feat: 헤더관련 상수 정의

---
+++ src/constant/stringConstant.ts
@@ -0,0 +1,19 @@
+export const HEADER_TITLE = {
+  LIST_PAGE: "Reservation",
+  CREATE_PAGE: "New Reservation",
+  EDIT_PAGE: "Edit Reservation",
+};
+export const FORM_PLACEHOLDER = {
+  NAME: "name",
+  PHONE: "phone",
+  DATE: "Select Date",
+  Guest: "Guests",
+  TABLE: "Select Table",
+  NOTE: "Add Note...",
+  SAVE: "Save",
+};
+export const HEADER_TYPE = {
+  LIST_PAGE: "list",
+  CREATE_PAGE: "create",
+  EDIT_PAGE: "edit",
+};
