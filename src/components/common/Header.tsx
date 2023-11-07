aecd146fa41217cf3f3cdafef40a2b2da500eaf6
ChoByungHyun <cbj973@gmail.com>
2023-11-08 02:29:20 +0900

    feat: 헤더 버튼 기능추가, mockUserData 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import BackButtonIcon from "assets/keyboard_backspace.svg";
+import { useNavigate } from "react-router-dom";
 import AddButtonIcon from "assets/add.svg";
 import CloseButtonIcon from "assets/close.svg";
 import { HEADER_TITLE, HEADER_TYPE } from "constant/stringConstant";
@@ -9,27 +10,33 @@ type Props = {
   pageType: string;
 };
 const Header = ({ pageType }: Props) => {
+  const navigate = useNavigate();
+  function navigateCreatePage() {
+    navigate("/create");
+  }
+  function navigateBack() {
+    navigate(-1);
+  }
   return (
     <SLayout>
       {pageType === HEADER_TYPE.LIST_PAGE ? (
         <>
-          <SBackButton>
-            <SImgButton src={AddButtonIcon} aria-label="뒤로가기"></SImgButton>
+          <SBackButton onClick={navigateCreatePage}>
+            <SImgButton src={AddButtonIcon} aria-label="예약생성"></SImgButton>
             <SCreateButton>{HEADER_TITLE.CREATE_PAGE}</SCreateButton>
           </SBackButton>
-
-          <STitle aria-label="폼 제목">{HEADER_TITLE.LIST_PAGE}</STitle>
+          <SMainTitle aria-label="폼 제목">{HEADER_TITLE.LIST_PAGE}</SMainTitle>
         </>
       ) : pageType === HEADER_TYPE.CREATE_PAGE ? (
         <>
-          <SBackButton>
+          <SBackButton onClick={navigateBack}>
             <SImgButton src={BackButtonIcon} aria-label="뒤로가기"></SImgButton>
           </SBackButton>
           <STitle aria-label="폼 제목">{HEADER_TITLE.CREATE_PAGE}</STitle>
         </>
       ) : (
         <>
-          <SBackButton>
+          <SBackButton onClick={navigateBack}>
             <SImgButton src={BackButtonIcon} aria-label="뒤로가기"></SImgButton>
           </SBackButton>
 
@@ -40,6 +47,7 @@ const Header = ({ pageType }: Props) => {
     </SLayout>
   );
 };
+
 const SCreateButton = styled.p`
   color: var(--primary);
 `;
@@ -50,17 +58,23 @@ const SBackButton = styled.div`
   padding: 8px;
   box-shadow: 2px 3px 5px 0px rgba(0, 0, 0, 0.1);
   background-color: var(--gray-200);
+  cursor: pointer;
 `;
 
 const STitle = styled.h1`
   font-size: 24px;
 `;
+const SMainTitle = styled(STitle)`
+  flex: 1;
+  text-align: center;
+`;
 
 const SLayout = styled.div`
   display: flex;
   justify-content: space-between;
+  box-sizing: border-box;
   width: 100%;
-  height: 60px;
+  height: 70px;
   margin: 0 auto;
   box-sizing: border-box;
   align-items: center;
