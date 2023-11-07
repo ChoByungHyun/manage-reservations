df50d27bbcec35c0b5b71091dbedd0b0e6967afd
ChoByungHyun <cbj973@gmail.com>
2023-11-07 22:02:58 +0900

    feat: 인풋 컴포넌트 구현

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,7 +1,31 @@
 import React from "react";
-
-const Input = () => {
-  return <div></div>;
+import styled from "styled-components";
+type Props = {
+  type?: "text" | undefined;
+  placeholder?: string;
+  label: string;
 };
+const Input: React.FC<Props> = ({ label, placeholder, type }) => {
+  return (
+    <SInputForm>
+      <label htmlFor="">{label}</label>
+      <SInput placeholder={placeholder} type={type}></SInput>
+    </SInputForm>
+  );
+};
+const SInputForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  font-size: 12px;
+  label {
+    color: var(--gray-600);
+  }
+`;
+
+const SInput = styled.input`
+  padding: 16px 80px 16px 16px;
+  border: 1px solid var(--gray-400);
+  border-radius: 5px;
+`;
 
 export default Input;
