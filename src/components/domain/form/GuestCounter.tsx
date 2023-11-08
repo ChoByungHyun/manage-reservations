3ecda40677393f8360710457c8b39d548b11b987
ChoByungHyun <cbj973@gmail.com>
2023-11-09 03:21:48 +0900

    feat: 게스트카운터 함수 disable 구현

--- src/components/domain/form/GuestCounter.tsx
+++ src/components/domain/form/GuestCounter.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import plusIcon from "assets/math-plus.svg";
 import minusIcon from "assets/math-minus.svg";
@@ -11,11 +11,11 @@ const GuestCounter: React.FC<Props> = ({ onCount, guest }) => {
   return (
     <SLayout>
       <div>{FORM_PLACEHOLDER.GUEST}</div>
-      <SIconLayout onClick={() => onCount(-1)}>
+      <SIconLayout $disabled={guest === 1} onClick={() => onCount(-1)}>
         <img src={minusIcon} alt="마이너스 아이콘" />
       </SIconLayout>
       <p>{guest}</p>
-      <SIconLayout onClick={() => onCount(1)}>
+      <SIconLayout $disabled={guest === 99} onClick={() => onCount(1)}>
         <img src={plusIcon} alt="플러스 아이콘" />
       </SIconLayout>
     </SLayout>
@@ -33,7 +33,10 @@ const SLayout = styled.div`
     color: var(--gray-800);
   }
 `;
-const SIconLayout = styled.div`
+interface IconProps {
+  $disabled: boolean;
+}
+const SIconLayout = styled.div<IconProps>`
   width: 40px;
   height: 40px;
   border: 1px solid var(--gray-400);
@@ -41,5 +44,7 @@ const SIconLayout = styled.div`
   box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.1);
   background-color: var(--gray-200);
   padding: 5px;
+  cursor: ${(props) => (props.$disabled ? "not-allowed" : "pointer")};
+  opacity: ${(props) => (props.$disabled ? 0.5 : 1)};
 `;
 export default GuestCounter;
