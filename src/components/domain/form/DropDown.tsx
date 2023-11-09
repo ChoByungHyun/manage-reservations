01c5111ae4dedcc034d45ef027d3dbb986a62eca
ChoByungHyun <cbj973@gmail.com>
2023-11-09 17:58:46 +0900

    feat: 테이블 드롭박스 선택 기능 구현

--- src/components/domain/form/DropDown.tsx
+++ src/components/domain/form/DropDown.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import ArrowIcon from "assets/arrow_drop_down.svg";
 import { FORM_PLACEHOLDER } from "constant/stringConstant";
+import TableTagButton from "./TableTagButton";
 
 type DropdownItem = {
   name: string;
@@ -15,19 +16,63 @@ type DropdownProps = {
 function DropDown({ items }: DropdownProps) {
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
 
-  const [selectedItem, setSelectedItem] = useState<string>(
-    FORM_PLACEHOLDER.TABLE
-  );
+  const [selectedItems, setSelectedItems] = useState<DropdownItem[]>([]);
+
+  const dropdownRef = useRef<HTMLDivElement>(null); // dropdownRef 생성
+
+  // 외부 클릭 감지
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleItemClick = (item: DropdownItem) => {
+    if (
+      selectedItems.length < 3 &&
+      !selectedItems.some((selectedItem) => selectedItem.id === item.id)
+    ) {
+      setSelectedItems([...selectedItems, item]);
+    }
+  };
+  const handleTagClick = (item: DropdownItem) => (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setSelectedItems(
+      selectedItems.filter((selectedItem) => selectedItem.id !== item.id)
+    );
+  };
 
   return (
-    <SDropdownContainer>
+    <SDropdownContainer ref={dropdownRef}>
       <SButton
         onClick={() => {
           setIsDropdownOpen((prev) => !prev);
         }}
         $isOpen={isDropdownOpen}
       >
-        {selectedItem}
+        <STagButtonLayout>
+          {selectedItems.length > 0
+            ? selectedItems.map((item) => (
+                <TableTagButton
+                  key={item.id}
+                  text={item}
+                  active
+                  showCloseButton // SButton에 있는 태그에만 SCloseButton 보이도록
+                  onClick={handleTagClick(item)} // SCloseButton 클릭 시 해당 태그 제거
+                />
+              ))
+            : FORM_PLACEHOLDER.TABLE}
+        </STagButtonLayout>
         <SArrowIconWrapper $isOpen={isDropdownOpen}>
           <img src={ArrowIcon} alt="드롭다운" />
         </SArrowIconWrapper>
@@ -35,16 +80,13 @@ function DropDown({ items }: DropdownProps) {
 
       <SDropdownList $isOpen={isDropdownOpen}>
         {items.map((item) => (
-          <SDropdownItemWrapper
-            $isOpen={isDropdownOpen}
-            key={item.id}
-            onClick={() => {
-              setSelectedItem(item.name);
-              setIsDropdownOpen(false);
-            }}
-          >
-            {item.name}
-          </SDropdownItemWrapper>
+          <TableTagButton
+            text={item}
+            active={selectedItems.some(
+              (selectedItem) => selectedItem.id === item.id
+            )}
+            onClick={() => handleItemClick(item)}
+          />
         ))}
       </SDropdownList>
     </SDropdownContainer>
@@ -54,7 +96,10 @@ function DropDown({ items }: DropdownProps) {
 type DropdownListProps = {
   $isOpen: boolean;
 };
-
+const STagButtonLayout = styled.div`
+  display: flex;
+  gap: 10px;
+`;
 const SDropdownContainer = styled.div`
   border-radius: 5px;
 `;
@@ -67,14 +112,15 @@ const SButton = styled.button<DropdownListProps>`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  gap: 20px;
   padding: 16px;
   color: var(--gray-800);
   ${(props) => props.$isOpen && `border : 2px solid var(--primary)`}
 `;
 
 const SArrowIconWrapper = styled.div<DropdownListProps>`
-  /* transform: ${({ $isOpen }) =>
-    $isOpen ? "rotate(0deg)" : "rotate(180deg"}; */
+  transform: rotate(${({ $isOpen }) => ($isOpen ? "180deg" : "0deg")});
+  transition: transform 0.3s ease;
 `;
 
 const SDropdownList = styled.div<DropdownListProps>`
@@ -82,11 +128,15 @@ const SDropdownList = styled.div<DropdownListProps>`
   border: 1px solid var(--gray-400);
   width: 540px;
   border-radius: 5px;
-  background-color: var(--gray-200);
+  background-color: var(--white);
+  padding: 10px 30px;
+  box-sizing: border-box;
   ${(props) =>
     props.$isOpen
       ? `
-   display:block;
+   display:flex;
+   flex-wrap:wrap;
+   gap:10px;
   `
       : `display:none;`}
 `;
