0a15e279621e36b031ce2983c6c558cad4feae01
ChoByungHyun <cbj973@gmail.com>
2023-11-09 22:54:26 +0900

    feat: 테이블 랜더링 컴포넌트 구현

---
+++ src/components/domain/form/table/renderTableData.tsx
@@ -0,0 +1,41 @@
+import { TableInfo } from "types/userType";
+import styled from "styled-components";
+
+export const DOT = "•";
+
+const renderTableData = (tableInfo: TableInfo[]) => {
+  const floorTableMap = tableInfo.reduce((acc, curr) => {
+    if (!acc[curr.floor]) {
+      acc[curr.floor] = [];
+    }
+    acc[curr.floor].push(curr.table);
+    return acc;
+  }, {} as Record<number, number[]>);
+
+  return Object.entries(floorTableMap).map(([floor, tables], index) => (
+    <SLayout key={index}>
+      Table
+      <STableNumber>
+        {tables.sort((a, b) => a - b).join(", ")}
+      </STableNumber>{" "}
+      {DOT} Floor<SFloorNumber> {floor}</SFloorNumber>
+    </SLayout>
+  ));
+};
+const SLayout = styled.div`
+  display: flex;
+  gap: 5px;
+  justify-content: center;
+  align-items: center;
+`;
+const STableNumber = styled.span`
+  font-size: 18px;
+  font-weight: bold;
+  color: var(--gray-1000);
+`;
+
+const SFloorNumber = styled.span`
+  /* font-weight: bold; */
+`;
+
+export default renderTableData;
