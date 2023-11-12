65f22221b5189a451f571688af7d79e96c19f7f4
ChoByungHyun <cbj973@gmail.com>
2023-11-12 13:16:44 +0900

    fix: 예약카드 정렬 안되는 버그 수정

--- src/util/sortReservationTime.ts
+++ src/util/sortReservationTime.ts
@@ -4,25 +4,19 @@ import { UserInfo } from "types/userType";
 export function sortReservationTime(reservations: UserInfo[]): UserInfo[] {
   return [...reservations].sort((a, b) => {
     const dateA = new Date(a.date.date);
+    dateA.setHours(0, 0, 0, 0); // 시간, 분, 초, 밀리초를 0으로 설정
     const dateB = new Date(b.date.date);
+    dateB.setHours(0, 0, 0, 0); // 시간, 분, 초, 밀리초를 0으로 설정
 
     if (dateA < dateB) {
       return -1;
     } else if (dateA > dateB) {
       return 1;
     } else {
-      const timeA = a.date.time
-        .split(":")
-        .reduce(
-          (acc, cur, idx) => acc + (idx === 0 ? Number(cur) * 60 : Number(cur)),
-          0
-        );
-      const timeB = b.date.time
-        .split(":")
-        .reduce(
-          (acc, cur, idx) => acc + (idx === 0 ? Number(cur) * 60 : Number(cur)),
-          0
-        );
+      const [hourA, minuteA] = convertTimeToHourAndMinute(a.date.time);
+      const [hourB, minuteB] = convertTimeToHourAndMinute(b.date.time);
+      const timeA = hourA * 60 + minuteA;
+      const timeB = hourB * 60 + minuteB;
 
       return timeA - timeB;
     }
@@ -30,6 +24,7 @@ export function sortReservationTime(reservations: UserInfo[]): UserInfo[] {
 }
 
 export function convertTimeToHourAndMinute(time: string): [number, number] {
+  // String으로 된 1:00 PM 식의 시간 변환 함수
   const [hourMin, period] = time.split(" ");
   const [hour, minute] = hourMin.split(":").map(Number);
 
